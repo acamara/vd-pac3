e62ef275cea6596385be76dcae2f4c7a782a3f26
Albert Cámara <albertcamara@hotmail.es>
2021-12-08 18:03:41 +0100

    Update Images + Add Footer + Add End info

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ $(".project").each(function(){
     
     var picOverlay = $(this).find(".overlay");
     var projectInfo = $(this).find(".project-info");
-    var smallTitle = $(this).find(".small-title");
     var projectLink = $(this).find(".project-link");
     var title = $(this).find("h4");
   
@@ -12,23 +11,34 @@ $(".project").each(function(){
         
     animateIn
     .fromTo(picOverlay, 2, {skewX:30, scale:1.5}, {skewX:0, xPercent:100, transformOrigin: "0% 100%", ease: Power4.easeOut},"-=1")
-
     .from(projectInfo, 1, {scaleY:0, transformOrigin: "bottom left"}, "-=1.5")
-
-    .from(smallTitle, 0.3, {autoAlpha: 0, y:30, ease: Power4.easeOut}, "-=0.8")
-
     .from(projectLink, 0.3, {autoAlpha: 0, y:30, ease: Power4.easeOut}, "-=0.8")
-
     .from(title, 0.3, {autoAlpha: 0, y:30, ease: Power4.easeOut}, "-=0.8")
 
-
     // Make a scrollMagic Scene
-
     var scene = new ScrollMagic.Scene({
         triggerElement: this,
     })
-
-    .addIndicators()
+    //.addIndicators()
     .setTween(animateIn).addTo(controller);
+});
 
+$(".project-end").each(function(){
+    var projectInfo = $(this).find(".project-info-center");
+    var projectLink = $(this).find(".project-link");
+    var title = $(this).find("h4");
+  
+    var animateIn = new TimelineMax();
+        
+    animateIn
+    .from(projectInfo, 1, {scaleY:0, transformOrigin: "bottom left"}, "-=1.5")
+    .from(projectLink, 0.3, {autoAlpha: 0, y:30, ease: Power4.easeOut}, "-=0.8")
+    .from(title, 0.3, {autoAlpha: 0, y:30, ease: Power4.easeOut}, "-=0.8")
+
+    // Make a scrollMagic Scene
+    var scene = new ScrollMagic.Scene({
+        triggerElement: this,
+    })
+    //.addIndicators()
+    .setTween(animateIn).addTo(controller);
 });
